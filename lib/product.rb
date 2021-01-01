dbd17cf499bcd09e2d13fe3bc367ec373535bd63
Olivia Charles <ocharles92@gmail.com>
2021-01-01 18:16:35 -0500

    Added hoarded it I1

--- lib/product.rb
+++ lib/product.rb
@@ -2,17 +2,28 @@ class Product
   attr_reader :category,
               :name,
               :unit_price,
-              :quantity
+              :quantity,
+              :is_hoarded
 
   def initialize(category, name, unit_price, quantity)
     @category   = category
     @name       = name
     @unit_price = unit_price
     @quantity   = quantity.to_i
+    @hoarded    = false
+
   end
 
 
   def total_price
     @unit_price * @quantity
+  end
+
+  def is_hoarded?
+    @hoarded
+  end
+
+  def hoard
+    @hoarded = true
   end
 end
--- test/product_test.rb
+++ test/product_test.rb
@@ -34,11 +34,20 @@ class ProductTest < Minitest::Test
   end
 
   def test_it_can_get_total_price
-    
+
     product1 = Product.new(:paper, 'toilet paper', 3.70, '10')
     product2 = Product.new(:meat, 'chicken', 4.50, '2')
 
     assert_equal 37.0, product1.total_price
     assert_equal 9.0, product2.total_price
+  end
+
+  def test_product_is_hoarded?
+    product = Product.new(:paper, 'toilet paper', 3.70, '10')
+
+
+  assert_equal false, product.is_hoarded?
+  product.hoard
+  assert_equal  true, product.is_hoarded?
   end
 end

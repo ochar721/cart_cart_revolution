b3877928aff61f23d4e9b9ef495e210156399336
Olivia Charles <ocharles92@gmail.com>
2021-01-01 20:27:20 -0500

    Completed percentafe and sorted

--- lib/shoppingcart.rb
+++ lib/shoppingcart.rb
@@ -38,6 +38,19 @@ class ShoppingCart
   def products_by_category(category)
     @products.find_all do |product|
       product.category == category
+     end
+  end
+
+
+  def percentage_occupied
+   (((total_number_of_products.to_f / capacity))*100).round(2)
+  end
+
+  def sorted_products_by_quantity
+    @products.sort_by do |product|
+      -product.quantity
     end
   end
+
+  
 end
--- test/shoppingcart_test.rb
+++ test/shoppingcart_test.rb
@@ -98,4 +98,48 @@ class ShoppingCartTest < Minitest::Test
   end
 
 
+  def test_percentage_occupied
+    cart = ShoppingCart.new("King Soopers", "30items")
+    product1 = Product.new(:paper, 'toilet paper', 3.70, '10')
+    product2 = Product.new(:meat, 'chicken', 4.50, '2')
+    product3 = Product.new(:paper, 'tissue paper', 1.25, '1')
+
+    cart.add_product(product1)
+    cart.add_product(product2)
+    cart.add_product(product3)
+
+    assert_equal 43.33, cart.percentage_occupied
+  end
+
+  def test_sorted_products_by_quantity
+    cart = ShoppingCart.new("King Soopers", "30items")
+    product1 = Product.new(:paper, 'toilet paper', 3.70, '10')
+    product2 = Product.new(:meat, 'chicken', 4.50, '2')
+    product3 = Product.new(:paper, 'tissue paper', 1.25, '1')
+    product4 = Product.new(:produce, 'apples', 0.99, '20')
+
+    cart.add_product(product1)
+    cart.add_product(product2)
+    cart.add_product(product3)
+    cart.add_product(product4)
+
+    assert_equal [product4, product1, product2, product3], cart.sorted_products_by_quantity
+  end
+
+  def test_sorted_products_by_quantity
+    cart = ShoppingCart.new("King Soopers", "30items")
+    product1 = Product.new(:paper, 'toilet paper', 3.70, '10')
+    product2 = Product.new(:meat, 'chicken', 4.50, '2')
+    product3 = Product.new(:paper, 'tissue paper', 1.25, '1')
+    product4 = Product.new(:produce, 'apples', 0.99, '20')
+
+    cart.add_product(product1)
+    cart.add_product(product2)
+    cart.add_product(product3)
+    cart.add_product(product4)
+
+    assert_equal {:meat => [product2], :paper => [product1, product3], :produce =>[produce4]}, cart.product_breakdown
+
+
+
 end
